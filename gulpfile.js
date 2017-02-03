c6ab094afe1fd2ec0cede28bd723681e080e26bd
daniel <xyztdanid4@gmail.com>
2017-02-03 12:26:37 +0100

    add

--- gulpfile.js
+++ gulpfile.js
@@ -52,26 +52,26 @@ gulp.task('minify-js', function() {
 });
 
 // Copy vendor libraries from /node_modules into /vendor
-// gulp.task('copy', function() {
-//     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-//         .pipe(gulp.dest('vendor/bootstrap'))
-//
-//     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-//         .pipe(gulp.dest('vendor/jquery'))
-//
-//     gulp.src([
-//             'node_modules/font-awesome/**',
-//             '!node_modules/font-awesome/**/*.map',
-//             '!node_modules/font-awesome/.npmignore',
-//             '!node_modules/font-awesome/*.txt',
-//             '!node_modules/font-awesome/*.md',
-//             '!node_modules/font-awesome/*.json'
-//         ])
-//         .pipe(gulp.dest('vendor/font-awesome'))
-// })
+gulp.task('copy', function() {
+    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+        .pipe(gulp.dest('vendor/bootstrap'))
+
+    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest('vendor/jquery'))
+
+    gulp.src([
+            'node_modules/font-awesome/**',
+            '!node_modules/font-awesome/**/*.map',
+            '!node_modules/font-awesome/.npmignore',
+            '!node_modules/font-awesome/*.txt',
+            '!node_modules/font-awesome/*.md',
+            '!node_modules/font-awesome/*.json'
+        ])
+        .pipe(gulp.dest('vendor/font-awesome'))
+})
 
 // Run everything
-gulp.task('default', ['less', 'minify-css', 'minify-js']);
+gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
@@ -94,12 +94,12 @@ gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() 
 
 // Compiles SCSS files from /scss into /css
 // NOTE: This theme uses LESS by default. To swtich to SCSS you will need to update this gulpfile by changing the 'less' tasks to run 'sass'!
-// gulp.task('sass', function() {
-//     return gulp.src('scss/agency.scss')
-//         .pipe(sass())
-//         .pipe(header(banner, { pkg: pkg }))
-//         .pipe(gulp.dest('css'))
-//         .pipe(browserSync.reload({
-//             stream: true
-//         }))
-// });
+gulp.task('sass', function() {
+    return gulp.src('scss/agency.scss')
+        .pipe(sass())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
