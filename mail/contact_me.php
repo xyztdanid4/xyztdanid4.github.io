92cb0a41da06c2596dc57512a14fcf95d027abe1
daniel <xyztdanid4@gmail.com>
2017-03-22 20:51:47 +0100

    asd

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -9,18 +9,18 @@ if(empty($_POST['name'])      ||
    echo "No arguments Provided!";
    return false;
    }
-   
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-   
+
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$to = 'ocsaigyrosgyar@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Üzenet az Ócsai Gyros Gyár honlapról";
+$email_body = "Üzenetet kaptál a honlap e-mail küldő felületéről!\n\n"."Részletek:\n\nNév: $name\n\nE-mail: $email_address\n\nTelefonszám: $phone\n\nÜzenet:\n$message";
+$headers = "From: noreply@gyrosgyar.hu\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-return true;         
+return true;
 ?>
